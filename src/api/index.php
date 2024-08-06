0ac8226f80f822a53745ddc40372b155aa455189
Jonathan <amobijonathan2@gmail.com>
2024-08-06 16:03:04 +0100

    CHORE: deleted redundant api folders

--- src/api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-
-
-include ("connect.php");
-echo ("hellooooooo worldddddddddd");
