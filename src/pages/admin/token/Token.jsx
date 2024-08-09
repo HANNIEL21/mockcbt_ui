46e9ab6ec4dbbead1bc550ed63fa6c1c534ff3df
Jonathan <amobijonathan2@gmail.com>
2024-08-09 06:29:42 +0100

    FEAT: Only Super Admins can edit tokens

--- src/pages/admin/token/Token.jsx
+++ src/pages/admin/token/Token.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import Table from "../../../components/Table";
-import { MdDelete, MdEdit, MdAdd, MdImportExport } from "react-icons/md";
+import { MdDelete, MdEdit, MdAdd } from "react-icons/md";
 import EditToken from "./EditToken";
 
-import Loader from "../../../components/Loader";
-import ImportToken from "./ImportToken";
 import AddToken from "./AddToken";
 import GenerateTokens from "./GenerateTokens";
 import DeleteToken from "./DeleteToken";
 import ModalOverlay from "../../../components/ModalOverlay";
 import { useTokens } from "../../../hooks/useTokens";
+import { useSelector } from "react-redux";
 
 const Token = () => {
   const [isOpenAddModal, setIsOpenAddModal] = useState(false);
   const [isOpenGenerateModal, setIsOpenGenerateModal] = useState(false);
-  const [isOpenImportModal, setIsOpenImportModal] = useState(false);
   const [isOpenEditModal, setIsOpenEditModal] = useState(false);
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
 
   const { data: tokens, isPending } = useTokens();
+  const { userDetails } = useSelector((state) => state.user);
+
+  console.log(userDetails);
 
   const openModal = (modalName, qId) => {
     switch (modalName) {
@@ -28,9 +29,6 @@ const Token = () => {
         break;
       case "generate":
         setIsOpenGenerateModal(true);
-        break;
-      case "import":
-        setIsOpenImportModal(true);
         break;
       case "edit":
         setSelectedId(qId);
@@ -53,9 +51,6 @@ const Token = () => {
         break;
       case "generate":
         setIsOpenGenerateModal(false);
-        break;
-      case "import":
-        setIsOpenImportModal(false);
         break;
       case "edit":
         setIsOpenEditModal(false);
@@ -101,48 +96,51 @@ const Token = () => {
       selector: (row) => row.created_at,
       sortable: true,
     },
-    {
-      name: "ACTIONS",
-      cell: (row) => (
-        <div className="flex gap-4">
-          <>
-            <button
-              onClick={() => openModal("edit", row.id)}
-              className="border-2 border-green-700 hover:bg-green-300 text-white font-bold text-sm rounded-md px-1 py-1 focus:outline-none"
-            >
-              <MdEdit className="text-xl text-green-700" />
-            </button>
-
-            {isOpenEditModal && (
-              <ModalOverlay>
-                <EditToken
-                  closeEditTokenModal={() => closeModal("edit")}
-                  id={selectedId}
-                />
-              </ModalOverlay>
-            )}
-          </>
-          <>
-            <button
-              onClick={() => openModal("delete", row.id)}
-              className="border-2 border-red-700 hover:bg-red-300 text-white font-bold text-sm rounded-md px-1 py-1 focus:outline-none"
-            >
-              <MdDelete className="text-xl text-red-700" />
-            </button>
-
-            {isOpenDeleteModal && (
-              <ModalOverlay>
-                <DeleteToken
-                  closeDeleteTokenModal={() => closeModal("delete")}
-                  id={selectedId}
-                />
-              </ModalOverlay>
-            )}
-          </>
-        </div>
-      ),
-    },
   ];
+
+  const actions = {
+    name: "ACTIONS",
+    cell: (row) => (
+      <div className="flex gap-4">
+        <>
+          <button
+            onClick={() => openModal("edit", row.id)}
+            className="border-2 border-green-700 hover:bg-green-300 text-white font-bold text-sm rounded-md px-1 py-1 focus:outline-none"
+          >
+            <MdEdit className="text-xl text-green-700" />
+          </button>
+
+          {isOpenEditModal && (
+            <ModalOverlay>
+              <EditToken
+                closeEditTokenModal={() => closeModal("edit")}
+                id={selectedId}
+              />
+            </ModalOverlay>
+          )}
+        </>
+        <>
+          <button
+            onClick={() => openModal("delete", row.id)}
+            className="border-2 border-red-700 hover:bg-red-300 text-white font-bold text-sm rounded-md px-1 py-1 focus:outline-none"
+          >
+            <MdDelete className="text-xl text-red-700" />
+          </button>
+
+          {isOpenDeleteModal && (
+            <ModalOverlay>
+              <DeleteToken
+                closeDeleteTokenModal={() => closeModal("delete")}
+                id={selectedId}
+              />
+            </ModalOverlay>
+          )}
+        </>
+      </div>
+    ),
+  };
+
+  if (userDetails.role === "SA") columns.push(actions);
 
   return (
     <div className="bg-white h-full rounded-lg shadow-md p-4">
@@ -184,23 +182,6 @@ const Token = () => {
                   <ModalOverlay>
                     <GenerateTokens
                       closeGenerateModal={() => closeModal("generate")}
-                    />
-                  </ModalOverlay>
-                )}
-              </>
-
-              <>
-                <button
-                  onClick={() => openModal("import")}
-                  className="bg-blue-900 flex items-center gap-2 text-white font-bold text-sm rounded-md px-3 py-1 focus:outline-none"
-                >
-                  <MdImportExport className="" /> <p>Import Tokens</p>
-                </button>
-
-                {isOpenImportModal && (
-                  <ModalOverlay>
-                    <ImportToken
-                      closeImportModal={() => closeModal("import")}
                     />
                   </ModalOverlay>
                 )}
